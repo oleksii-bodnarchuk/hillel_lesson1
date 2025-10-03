172f00454564e792ea37ab66cab7b42c9e02dfcc
Oleksii Bodnarchuk <oleksii.bodnarchuk@gmail.com>
2025-10-03 21:16:14 +0300

    add lesson 2 home work

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello Oleksii!")
-
-x = input("type number: ")
+user_input = input('type a number: ') # Нехай користувач надрукував 4
+print(type(user_input)) # <class 'str'>
+number = float(user_input)
+print(number / 2) # Виведе 2
